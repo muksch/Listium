14fbb67c071f840bbb47b23d34f0e5e4cb2b7421
muksch <jan.muksch@gmail.com>
2025-11-11 09:03:35 +0100

    apikey fix

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { config } from 'dotenv';
+
+config();
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBk_cpDbw2Wnh6vqRfebHW7YXq9Y0Za_A',
-  authDomain: 'listium.firebaseapp.com',
-  projectId: 'listium',
-  storageBucket: 'listium.firebasestorage.app',
-  messagingSenderId: '122068480010',
-  appId: '1:122068480010:web:5d6ccbebe3701700f9eff3',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
